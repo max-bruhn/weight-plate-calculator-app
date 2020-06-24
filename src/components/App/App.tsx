a0a0ca23a260b3c39e55470921d2b6a576f35040
Max Bruhn <mpcbruhn+git@gmail.com>
2020-06-24 17:27:44 +0700

    added context and immer reducer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { useImmerReducer } from 'use-immer';
 
 // css
 import 'normalize.css';
@@ -7,15 +8,43 @@ import './../../tailwind.generated.css';
 import Header from '../Header/Header';
 import Content from '../Content/Content';
 
+type StateContextType = {
+  useMetric: boolean;
+};
+
+const StateContext = createContext<StateContextType | undefined>(undefined);
+const DispatchContext = createContext<any>(undefined);
+
 const App: React.FC = () => {
+  const initialState = {
+    useMetric: true,
+  };
+
+  const ourReducer = (draft: any, action: any) => {
+    switch (action.type) {
+      case 'useMetric':
+        draft.useMetric = action.value;
+        return;
+
+      default:
+        return;
+    }
+  };
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
   return (
     <>
-      <div className="bg-darker min-w-screen min-h-screen text-light">
-        <div className="container mx-auto">
-          <Header />
-          <Content />
-        </div>
-      </div>
+      <StateContext.Provider value={state}>
+        <DispatchContext.Provider value={dispatch}>
+          <div className="bg-darker min-w-screen min-h-screen text-light">
+            <div className="container mx-auto">
+              <Header />
+              <Content />
+            </div>
+          </div>
+        </DispatchContext.Provider>
+      </StateContext.Provider>
     </>
   );
 };
